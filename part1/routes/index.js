02778a8d60a7f24f84daf28f3c87ef2e83d33194
a1909807 <cxi9371@outlook.com>
2025-06-20 16:17:02 +0000

    [automatic commit] 2025-06-20 16:17:02 UTC 119.15.78.61

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -3,20 +3,19 @@ var router = express.Router();
 const db = require('../models/db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 
 
 /* GET Return a list of all dogs with their size and owner's username. page. */
-router.get('/api/dogs', async function(req, res, next) {
+router.get('/api/dogs', async function (req, res, next) {
   try {
-        const [result] = await db.query(`
-        SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
-        FROM Dogs JOIN Users
-          ON d.owner_id = u.user_id`);
-
+    const [result] = await db.query(`
+      SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+      FROM Dogs JOIN Users
+        ON d.owner_id = u.user_id`);
 
     res.status(200).json(result);
   } catch (error) {
@@ -25,9 +24,8 @@ router.get('/api/dogs', async function(req, res, next) {
 });
 
 
-
 /* Return all open walk requests, including the dog name, requested time, location, and owner's username. */
-router.get('/api/walkrequests/open', async function(req, res, next) {
+router.get('/api/walkrequests/open', async function (req, res, next) {
   try {
     const [result] = await db.query(`
     SELECT
@@ -54,7 +52,7 @@ router.get('/api/walkrequests/open', async function(req, res, next) {
 
 
 /* Return a summary of each walker with their average rating and number of completed walks. */
-router.get('/api/walkers/summary', async function(req, res, next) {
+router.get('/api/walkers/summary', async function (req, res, next) {
   try {
     const [rows] = await db.query(`
     SELECT
