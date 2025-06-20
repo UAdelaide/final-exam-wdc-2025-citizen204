b4aa6c0d7a93512f3d078b59cc92d15f5f20907f
a1909807 <cxi9371@outlook.com>
2025-06-20 23:19:23 +0000

    [automatic commit] 2025-06-20 23:19:23 UTC 119.15.78.61

--- part2/app.js
+++ part2/app.js
@@ -16,7 +16,7 @@ async function InitDB() {
       ('carol123', 'carol@example.com', 'hashed789', 'owner'),
       ('user1', 'user1@example.com', '666', 'walker'),
       ('user2', 'user2@example.com', '666', 'owner');
-      `);
+    `);
 
     await db.query(`
 INSERT INTO Dogs (owner_id, name, size) VALUES
@@ -25,11 +25,16 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'user2'), 'dog1', 'large'),
 ((SELECT user_id FROM Users WHERE username = 'user2'), 'dog2', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'dog3', 'medium');
-      `);
+    `);
 
     await db.query(`
-
-      `)
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+((SELECT dog_id FROM Dogs WHERE name = 'dog1' AND owner_id = (SELECT user_id FROM Users WHERE username = 'user2')), '2025-06-11 10:00:00', 60, 'City Park', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'dog2' AND owner_id = (SELECT user_id FROM Users WHERE username = 'user2')), '2025-06-11 14:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'dog3' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-12 16:00:00', 40, 'Beachside Ave', 'completed');
+    `);
 
   } catch (error) {
     console.log(error);
