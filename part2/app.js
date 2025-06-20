a376cb313110d9c86a7c9a6c22b37f8b1e57fffc
a1909807 <cxi9371@outlook.com>
2025-06-20 19:53:57 +0000

    [automatic commit] 2025-06-20 19:53:57 UTC 119.15.78.61

--- part2/app.js
+++ part2/app.js
@@ -6,15 +6,12 @@ const session = require('express-session');
 const app = express();
 
 // Middleware
+
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(session({
-  secret: 'your_secret_key', // 用于签名 session ID 的秘密字符串
-  resave: false, // 强制 session 保持初始化，即使它没有被修改
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
