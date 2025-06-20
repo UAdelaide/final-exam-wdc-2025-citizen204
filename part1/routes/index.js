e230bd45d72255324971954f86dcec966aadf4c8
a1909807 <cxi9371@outlook.com>
2025-06-20 16:01:57 +0000

    [automatic commit] 2025-06-20 16:01:57 UTC 119.15.78.61

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -12,8 +12,8 @@ router.get('/', function(req, res, next) {
 /* GET Return a list of all dogs with their size and owner's username. page. */
 router.get('/', function(req, res, next) {
   try {
-    const rows = db.query(``);
-    
+    const [rows] = await db.query(``);
+
   } catch (error) {
     res.status(500).json({ error: error });
   }
