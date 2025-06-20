4f53b02b866395536e14c45f0235657bf3b313ab
a1909807 <cxi9371@outlook.com>
2025-06-20 16:00:57 +0000

    [automatic commit] 2025-06-20 16:00:57 UTC 119.15.78.61

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const db = 
+const db = require('../models/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 /* GET Return a list of all dogs with their size and owner's username. page. */
 router.get('/', function(req, res, next) {
   try {
-    res.render('index', { title: 'Express' });
+    
   } catch (error) {
     res.status(500).json({ error: error });
   }
