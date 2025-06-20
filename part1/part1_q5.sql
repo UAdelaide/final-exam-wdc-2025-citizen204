a85084d385eb816f61c0b56c73fa8f9900668733
a1909807 <cxi9371@outlook.com>
2025-06-20 15:54:55 +0000

    [automatic commit] 2025-06-20 15:54:55 UTC 119.15.78.61

--- part1/part1_q5.sql
+++ part1/part1_q5.sql
@@ -21,5 +21,5 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 ((SELECT dog_id FROM Dogs WHERE name = 'dog1' AND owner_id = (SELECT user_id FROM Users WHERE username = 'user2')), '2025-06-11 10:00:00', 60, 'City Park', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'dog2' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-11 14:00:00', 30, 'Riverside Trail', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'dog3' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-12 16:00:00', 40, 'Greenwich Gardens', 'completed');
+((SELECT dog_id FROM Dogs WHERE name = 'dog2' AND owner_id = (SELECT user_id FROM Users WHERE username = 'user2')), '2025-06-11 14:00:00', 30, 'Riverside Trail', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'dog3' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-12 16:00:00', 40, 'Greenwich Gardens', 'completed');
