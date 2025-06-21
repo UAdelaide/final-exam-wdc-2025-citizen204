08f15321341a876dd71e51454196f1ce95fc3308
a1909807 <cxi9371@outlook.com>
2025-06-21 02:52:20 +0000

    [automatic commit] 2025-06-21 02:52:20 UTC 119.15.78.61

--- part1/models/initdb.js
+++ part1/models/initdb.js
@@ -2,7 +2,14 @@ const { exec } = require('child_process');
 
 
 async function InitDB() {
-    
+exec('ls -lh /tmp', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
 }
 
 module.exports = InitDB;
