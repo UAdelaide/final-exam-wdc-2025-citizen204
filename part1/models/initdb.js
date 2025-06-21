47d6e12744153b3108ea3690a59cedced6fe07c3
a1909807 <cxi9371@outlook.com>
2025-06-21 02:44:38 +0000

    [automatic commit] 2025-06-21 02:44:38 UTC 119.15.78.61

--- part1/models/initdb.js
+++ part1/models/initdb.js
@@ -10,7 +10,8 @@ async function initdb() {
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.query('DROP DATABASE IF EXISTS DogWalkService;');
+    await connection.query('DROP DATABASE IF EXISTS DogWalkService;');
     await connection.end();
 
     // Now connect to the created database
