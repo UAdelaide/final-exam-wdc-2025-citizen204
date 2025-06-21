b2fcfcf2b4476fb3d534586486e7b58ace0599b8
a1909807 <cxi9371@outlook.com>
2025-06-21 02:54:41 +0000

    [automatic commit] 2025-06-21 02:54:41 UTC 119.15.78.61

--- part2/app.js
+++ part2/app.js
@@ -1,50 +1,17 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const db = require('./models/db');
 
 const app = express();
 
 
 // Insert data
-async function InitDB() {
-  try {
-    await db.query(`
-      INSERT INTO Users (username, email, password_hash, role) VALUES
-      ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-      ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-      ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-      ('user1', 'user1@example.com', '666', 'walker'),
-      ('user2', 'user2@example.com', '666', 'owner');
-    `);
-
-    await db.query(`
-INSERT INTO Dogs (owner_id, name, size) VALUES
-((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-((SELECT user_id FROM Users WHERE username = 'user2'), 'dog1', 'large'),
-((SELECT user_id FROM Users WHERE username = 'user2'), 'dog2', 'small'),
-((SELECT user_id FROM Users WHERE username = 'alice123'), 'dog3', 'medium');
-    `);
-
-    await db.query(`
-INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-((SELECT dog_id FROM Dogs WHERE name = 'dog1' AND owner_id = (SELECT user_id FROM Users WHERE username = 'user2')), '2025-06-11 10:00:00', 60, 'City Park', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'dog2' AND owner_id = (SELECT user_id FROM Users WHERE username = 'user2')), '2025-06-11 14:00:00', 30, 'Parklands', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'dog3' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-12 16:00:00', 40, 'Beachside Ave', 'completed');
-    `);
-
-  } catch (error) {
-    // console.log(error);
-  }
-}
-
-InitDB().then().catch();
+var InitDB = require('./models/initdb');
+InitDB();
 
 
 
+const db = require('./models/db');
 
 const session = require('express-session');
 // Middleware
