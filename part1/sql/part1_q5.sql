0119346b60e93fa0d9a52ad42eb6b19b117bf380
a1909807 <cxi9371@outlook.com>
2025-06-21 02:43:58 +0000

    [automatic commit] 2025-06-21 02:43:58 UTC 119.15.78.61

--- part1/sql/part1_q5.sql
+++ part1/sql/part1_q5.sql
@@ -5,8 +5,8 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('user1', 'user1@example.com', '666', 'walker'),
-('user2', 'user2@example.com', '666', 'owner');
+('user1', 'user1@example.com', 'hashed666', 'walker'),
+('user2', 'user2@example.com', 'hashed666', 'owner');
 
 -- Insert five dogs
 INSERT INTO Dogs (owner_id, name, size) VALUES
