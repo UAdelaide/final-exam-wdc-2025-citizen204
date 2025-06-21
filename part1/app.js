0a4988012d82eef8d7e035a06c5db705dc2795eb
a1909807 <cxi9371@outlook.com>
2025-06-21 02:46:38 +0000

    [automatic commit] 2025-06-21 02:46:38 UTC 119.15.78.61

--- part1/app.js
+++ part1/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var initdb = require('./mo')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
